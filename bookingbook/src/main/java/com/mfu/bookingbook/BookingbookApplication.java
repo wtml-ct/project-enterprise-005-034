64565264955033b222a2f4aacb4ed44577c11e36
wtml-ct <6331305005@lamduan.mfu.ac.th>
2022-11-18 11:08:25 +0700

    First commit

---
+++ bookingbook/src/main/java/com/mfu/bookingbook/BookingbookApplication.java
@@ -0,0 +1,13 @@
+package com.mfu.bookingbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingbookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookingbookApplication.class, args);
+	}
+
+}
---
+++ bookingbook/src/test/java/com/mfu/bookingbook/BookingbookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mfu.bookingbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingbookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
