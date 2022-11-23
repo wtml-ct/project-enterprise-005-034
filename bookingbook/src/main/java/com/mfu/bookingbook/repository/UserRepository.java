ef8a5782a7a0a941c9a2ffee3dbae516a3aeed4c
wtml-ct <6331305005@lamduan.mfu.ac.th>
2022-11-23 12:37:53 +0700

    Repository V1

---
+++ bookingbook/src/main/java/com/mfu/bookingbook/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package com.mfu.bookingbook.repository;
+
+import com.mfu.bookingbook.domain.*;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+  List<User> findById(long id);
+}
+
