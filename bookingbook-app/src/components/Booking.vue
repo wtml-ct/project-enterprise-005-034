ba1f832f88b2e58368980baa4561ecdb076ddab4
BuF-FeR-034 <kaowfang0404@gmail.com>
2022-11-26 01:46:58 +0700

    frontV3

---
+++ bookingbook-app/src/components/Booking.vue
@@ -0,0 +1,65 @@
+<template>
+    <div id="app">
+  <v-app id="inspire">
+    <v-card>
+      <v-toolbar
+        color="cyan"
+        dark
+        flat
+      >
+       
+  
+        <v-toolbar-title>BookingBook</v-toolbar-title>
+  
+        <v-spacer></v-spacer>
+  
+        <v-btn icon>
+          <v-icon>mdi-plus</v-icon>
+          <v-title>Add</v-title>
+        </v-btn>
+  
+    
+        <template v-slot:extension>
+          <v-tabs
+            v-model="tab"
+            align-with-title
+          >
+            <v-tabs-slider color="yellow"></v-tabs-slider>
+  
+            <v-tab
+              v-for="item in items"
+              :key="item"
+            >
+              {{ item }}
+            </v-tab>
+          </v-tabs>
+        </template>
+      </v-toolbar>
+  
+      <v-tabs-items v-model="tab">
+        <v-tab-item
+          v-for="item in items"
+          :key="item"
+        >
+          <v-card flat>
+            <v-card-text v-text="text"></v-card-text>
+          </v-card>
+        </v-tab-item>
+      </v-tabs-items>
+    </v-card>
+  </v-app>
+    </div>
+</template>>
+<script>
+  export default {
+    data () {
+      return {
+        tab: null,
+        items: [
+          'web', 'shopping', 'videos', 'images', 'news',
+        ],
+      }
+    },
+  }
+</script>
+>
