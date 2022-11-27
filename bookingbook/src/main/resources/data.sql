c07cdb167964648598806c4f67fb3a4effe04199
wtml-ct <6331305005@lamduan.mfu.ac.th>
2022-11-28 00:28:02 +0700

    data.sql author

--- bookingbook/src/main/resources/data.sql
+++ bookingbook/src/main/resources/data.sql
@@ -1,9 +1,9 @@
 -- USER
-INSERT INTO USERS (USER_ID, USERNAME, PASSWORD) VALUES (1, 'user', 'user');
+INSERT INTO USERS (USER_ID,NAME, USERNAME, PASSWORD) VALUES (1,'User1', 'user1', 'user1');
 
 -- AUTHOR
-INSERT INTO AUTHOR (AUTHOR_ID, NAME) VALUES (1, 'wtml');
-INSERT INTO AUTHOR (AUTHOR_ID, NAME) VALUES (2, 'kf');
+INSERT INTO AUTHOR (AUTHOR_ID, NAME,PIC) VALUES (1, 'wtml', 'https://images.indianexpress.com/2021/09/Untitled-design-22.jpg');
+INSERT INTO AUTHOR (AUTHOR_ID, NAME,PIC) VALUES (2, 'kf','https://www.geo.tv/assets/uploads/updates/2022-03-20/406373_6434386_updates.jpg');
 
 -- BOOK
 
