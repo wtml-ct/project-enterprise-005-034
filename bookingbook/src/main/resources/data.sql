74a56ec4b995a4962deaa3b6729677691f3cd59b
wtml-ct <6331305005@lamduan.mfu.ac.th>
2022-11-29 05:05:11 +0700

    Front 29/11

--- bookingbook/src/main/resources/data.sql
+++ bookingbook/src/main/resources/data.sql
@@ -7,8 +7,8 @@ INSERT INTO AUTHOR (AUTHOR_ID, NAME, PIC) VALUES (1, 'wtml', 'https://images.ind
 INSERT INTO AUTHOR (AUTHOR_ID, NAME, PIC) VALUES (2, 'kf','https://www.geo.tv/assets/uploads/updates/2022-03-20/406373_6434386_updates.jpg');
 
 -- BOOK
-INSERT INTO BOOK (BOOK_ID, TITLE, IMAGE, DESCRIPTION, AUTHOR_ID) VALUES (1, 'TITLE1', 'https://www.coderus.com/wp-content/uploads/2020/11/different-types-of-software-coderus-branded-image.jpg', 'Software', 1);
-INSERT INTO BOOK (BOOK_ID, TITLE, IMAGE, DESCRIPTION, AUTHOR_ID) VALUES (2, 'TITLE2', 'https://www.coderus.com/wp-content/uploads/2020/11/different-types-of-software-coderus-branded-image.jpg', 'Software', 1);
-
+INSERT INTO BOOK (BOOK_ID, TITLE, IMAGE, DESCRIPTION, AUTHOR_ID) VALUES (1, 'Software', 'https://www.coderus.com/wp-content/uploads/2020/11/different-types-of-software-coderus-branded-image.jpg', 'Software', 1);
+INSERT INTO BOOK (BOOK_ID, TITLE, IMAGE, DESCRIPTION, AUTHOR_ID) VALUES (2, 'Media', 'https://www.coderus.com/wp-content/uploads/2020/11/different-types-of-software-coderus-branded-image.jpg', 'Media', 1);
+INSERT INTO BOOK (BOOK_ID, TITLE, IMAGE, DESCRIPTION, AUTHOR_ID) VALUES (3, 'English', 'https://www.coderus.com/wp-content/uploads/2020/11/different-types-of-software-coderus-branded-image.jpg', 'English', 2);
 -- BOOKING
 INSERT INTO BOOKING (BOOKING_ID, DATE, USER_ID, BOOK_ID) VALUES (1,'10', 1, 1) 
