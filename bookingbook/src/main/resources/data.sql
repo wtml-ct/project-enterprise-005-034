dbdd6427f68995a9e28b433ae6e6b2504aeeec6c
wtml-ct <6331305005@lamduan.mfu.ac.th>
2022-11-29 21:16:53 +0700

    data

--- bookingbook/src/main/resources/data.sql
+++ bookingbook/src/main/resources/data.sql
@@ -7,8 +7,9 @@ INSERT INTO AUTHOR (AUTHOR_ID, NAME, PIC) VALUES (1, 'wtml', 'https://images.ind
 INSERT INTO AUTHOR (AUTHOR_ID, NAME, PIC) VALUES (2, 'kf','https://www.geo.tv/assets/uploads/updates/2022-03-20/406373_6434386_updates.jpg');
 
 -- BOOK
-INSERT INTO BOOK (BOOK_ID, TITLE, IMAGE, DESCRIPTION, AUTHOR_ID) VALUES (1, 'Software', 'https://www.coderus.com/wp-content/uploads/2020/11/different-types-of-software-coderus-branded-image.jpg', 'Software', 1);
-INSERT INTO BOOK (BOOK_ID, TITLE, IMAGE, DESCRIPTION, AUTHOR_ID) VALUES (2, 'Media', 'https://www.coderus.com/wp-content/uploads/2020/11/different-types-of-software-coderus-branded-image.jpg', 'Media', 1);
-INSERT INTO BOOK (BOOK_ID, TITLE, IMAGE, DESCRIPTION, AUTHOR_ID) VALUES (3, 'English', 'https://www.coderus.com/wp-content/uploads/2020/11/different-types-of-software-coderus-branded-image.jpg', 'English', 2);
+INSERT INTO BOOK (BOOK_ID, TITLE, IMAGE, DESCRIPTION, AUTHOR_ID) VALUES (1, 'Software', 'https://www.vandanapublications.com/wp-content/uploads/2020/06/Front-5-scaled.jpg', 'Software', 1);
+INSERT INTO BOOK (BOOK_ID, TITLE, IMAGE, DESCRIPTION, AUTHOR_ID) VALUES (2, 'Media', 'https://thammasatpress.tu.ac.th/wp_tupress/wp-content/uploads/2021/08/64110-1-scaled.jpg', 'Media', 1);
+INSERT INTO BOOK (BOOK_ID, TITLE, IMAGE, DESCRIPTION, AUTHOR_ID) VALUES (3, 'English', 'https://lzd-img-global.slatic.net/g/p/757315de16135da9637aebf211eba9ca.jpg_720x720q80.jpg', 'English', 2);
+
 -- BOOKING
 INSERT INTO BOOKING (BOOKING_ID, DATE, USER_ID, BOOK_ID) VALUES (1,'10', 1, 1) 
